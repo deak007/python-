7fb384adc8c6b4fb9ed6b4eeb94fb610c5b2e7d2
deak007 <deakajtazi1@gmail.com>
2026-02-25 16:38:23 +0100

    Update session 2.py

--- session 2.py
+++ session 2.py
@@ -34,3 +34,138 @@ print(can+box)
 
 na = input("emri: ")
 print("hello,"+ na)
+
+grade = int(input("type in your score:"))
+if grade > 50:
+    print("you passed!")
+else:
+    print("you failed.")
+
+age1 = int(input("Your age: "))
+if age1 >= 18:
+    print("You are an adult")
+else:
+    print('You are a minor')
+
+
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+if number1 > number2:
+    print(number1 , "is bigger")
+elif number1 < number2:
+    print(number2 , "is bigger")
+else:
+    print("the two given numbers are equal.")
+
+grade1 = int(input("Your score: "))
+if 100 > grade1 > 90:
+    print("your grade is an A")
+elif 90 > grade1 > 80:
+    print("your grade is a B")
+elif 80 > grade1 > 60:
+    print("your grade is a C")
+elif 60 > grade1 > 30:
+    print("your grade is a D")
+else:
+    print("your grade is a F")
+
+temperature = int(input("Temperature: "))
+if temperature < 10:
+    print("it is cold")
+elif 10 < temperature < 25:
+    print( 'it is warm')
+else:
+    print("it is hot")
+
+username1 = ("deakj")
+password1 = ("citpycourse")
+username = input(" username: ")
+password = input(" password: ")
+if username1 == username + password1 == password:
+    print("access granted")
+else:
+    print("access denied")
+  
+nr1 = int(input("First number: "))
+nr2 = int(input("Second number: "))
+if nr1 > nr2:
+    print(nr1 , "is bigger")
+elif nr1 < nr2:
+    print(nr2 , "is bigger")
+else:
+    print( nr1 , "and" , nr2 , "are equal")
+
+nr1 = int(input("Number: "))
+if nr1 < 100:
+    print("this is a small number")
+elif 100 < nr1 < 1000:
+    print("this number is nor small nor big")
+else:
+    print("this is a big number")
+
+bool = True
+bool2 = False
+print(bool,bool2)
+
+print(5==3)
+print(5=="5")
+print(5!=3)
+print(5>3,5>3,5>=1,5<=3)
+
+if 10 > 5:
+    print("This statement is true")
+
+number = 5
+if number > 0:
+    print("we still have tickets remaining")
+
+age = -5
+if age > 18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
+
+age = 15
+if age > 18:
+    print("The prize is 500 lek")
+elif age > 10:
+    print("the prize is 200 lek")
+else:
+    print("You can enter for free")
+
+grade = 50
+if grade > 100:
+    print("invalid input")
+elif grade < 0:
+    print("invalid input")
+else:
+    print("valid")
+
+print(10==15)
+print(10!=12)
+print(10>2)
+print(2<5)
+print(10>=20)
+print(6<=15)
+
+v1=(2)
+if v1 > 10:
+    print("this statement is true")
+print(v1>10)
+
+v2=(4)
+v3=(6)
+print(v2==v3)
+if v2 == v3:
+    print("this statement is true")
+
+nr=6
+if nr > 0:
+    print("this number is positive")
+else:
+    print("this number is negative")
+
+
+
+
+
